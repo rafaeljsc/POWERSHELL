eb96d1a86eb2ad8b4495f47f8834b1364da08879
Rafael Costa <72453565+rafaeljsc@users.noreply.github.com>
2020-10-13 00:25:49 -0300

    Removedor de acentos
    Função para remover acentos em arrays de/ou strings.

---
+++ RemovedorDeAcentos.ps1
@@ -0,0 +1,25 @@
+﻿Remove-Variable * -ErrorAction SilentlyContinue
+cls
+function Remove-Acentos {
+param ([String]$src = [String]::Empty)
+  $normalized = $src.Normalize( [Text.NormalizationForm]::FormD )
+  $sb = new-object Text.StringBuilder
+  $normalized.ToCharArray() | ForEach-Object { 
+    if( [Globalization.CharUnicodeInfo]::GetUnicodeCategory($_) -ne [Globalization.UnicodeCategory]::NonSpacingMark) {
+      [void]$sb.Append($_)
+    }
+  }
+  $sb.ToString()
+}
+
+#EXEMPLO
+Do {
+""
+  [array]$name+= Read-Host 'Nome'
+  ""
+  $continue = Read-Host 'Adicionar mais nomes? (S/N)'
+
+}while ($continue.ToLower() -eq 's')
+
+""
+$name | ForEach-Object {Remove-Acentos $_}
