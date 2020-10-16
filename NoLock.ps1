0eea2b0a378bec675a2b0889f1c1e77d19545867
Rafael Costa <72453565+rafaeljsc@users.noreply.github.com>
2020-10-16 09:57:18 -0300

    Update NoLock.ps1

--- NoLock.ps1
+++ NoLock.ps1
@@ -1,8 +1,9 @@
+mode 40,10
 Write-Host NoLock ativado! -b Green
 ""
 Write-Host Feche esta janela para desativar -f Yellow.
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
-while ($true)
+while (!$loop)
 {
   [System.Windows.Forms.SendKeys]::SendWait("{CAPSLOCK 2}")
   sleep 60
