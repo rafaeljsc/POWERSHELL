48ad8b744f41c489deba692fdf2859aecc53a94e
Rafael Costa <72453565+rafaeljsc@users.noreply.github.com>
2021-01-26 11:11:47 -0300

    Cria credenciais criptografadas

---
+++ CredentialCreator.ps1
@@ -0,0 +1,23 @@
+﻿Remove-Variable * -ErrorAction SilentlyContinue
+cls
+
+$User = Read-Host 'Usuário'
+$UserPassword = Read-Host 'Senha' -AsSecureString
+$Cred = New-Object System.Management.Automation.PSCredential ("$User", $UserPassword)
+
+$Output = "$env:LOCALAPPDATA\Temp\Cred.txt"
+
+$Key = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Password = $UserPassword | ConvertFrom-SecureString -key $Key
+$Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, ($Password | ConvertTo-SecureString -Key $key)
+
+$Key = [string]$key -replace ' ',','
+
+$Credential = @(
+'$User = ' + """$User"""
+'$Password = ' + """$Password"""
+'$Key = ' + $Key
+'$Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, ($Password | ConvertTo-SecureString -Key $key)')
+
+$Credential | Out-File $Output | notepad $Output
