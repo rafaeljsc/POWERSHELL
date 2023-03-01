9358488e8057fabfa694882f80f99e96e14cfb46
Rafael Costa <72453565+rafaeljsc@users.noreply.github.com>
2023-03-01 14:15:24 -0300

    Add files via upload

---
+++ multithreading sample.ps1
@@ -0,0 +1,11 @@
+﻿cls
+$jobs = @()
+
+$urls = 'facebook.com','google.com','youtube.com','outlook.com'
+foreach ($url in $urls){
+    $jobs += Start-Job -Name Ping {param($url) Test-Connection $url} -ArgumentList $url
+}
+
+foreach ($job in $jobs){
+    Receive-Job $job -AutoRemoveJob -Wait
+}
